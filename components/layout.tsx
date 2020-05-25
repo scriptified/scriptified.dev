6d26c6a59321e81e5cb90682b042f0df48176f90
Prateek Surana <prateeksurana3255@gmail.com>
2020-05-26 02:05:34 +0530

    :recycle: Added typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,18 @@
-import Head from "next/head";
-import styles from "./layout.module.css";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-const name = "Something Amazing";
-export const siteTitle = "Next.js Sample Website";
+const name = '[Your Name]'
+export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -19,7 +25,7 @@ export default function Layout({ children, home }) {
           property="og:image"
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -62,5 +68,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  );
+  )
 }
---
+++ pages/issues/[id].tsx
@@ -0,0 +1,48 @@
+import Layout from '../../components/layout'
+import { getAllIssueIds, getIssueData } from '../../lib/issues'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
+
+export default function Issue({
+  issueData
+}: {
+  issueData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{issueData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{issueData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={issueData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: issueData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllIssueIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const issueData = await getIssueData(params.id as string)
+  return {
+    props: {
+      issueData
+    }
+  }
+}
