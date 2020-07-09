130786fcf65988a2f4780a6f32c1e3edf83071bd
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-09 12:16:50 +0530

    :sparkles: Added Code snippet component

---
+++ components/CodeSnippet.tsx
@@ -0,0 +1,28 @@
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import github from 'prism-react-renderer/themes/github';
+
+function CodeSnippet({ code, language }) {
+  return (
+    <Highlight {...defaultProps} code={code} language={language} theme={github}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre
+          className={`${className} text-left my-4 p-2 overflow-scroll rounded border border-solid border-gray-400`}
+          style={style}
+        >
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })} className="table-row" key={i}>
+              <span className="table-cell text-right pr-4 select-none opacity-50">{i + 1}</span>
+              <span className="table-cell">
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} key={key} />
+                ))}
+              </span>
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+}
+
+export default CodeSnippet;
