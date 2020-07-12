6f1909412094c725b3c362a06e2f716b7df176d7
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-12 16:00:58 +0530

    :recycle: Make Social links nullable

--- interfaces/social.ts
+++ interfaces/social.ts
@@ -1,10 +1,10 @@
 export default interface Social {
-  website: string;
-  blog: string;
-  twitter: string;
-  github: string;
-  youTube: string;
-  linkedIn: string;
-  instagram: string;
-  other: string;
+  website?: string;
+  blog?: string;
+  twitter?: string;
+  github?: string;
+  youTube?: string;
+  linkedIn?: string;
+  instagram?: string;
+  other?: string;
 }
