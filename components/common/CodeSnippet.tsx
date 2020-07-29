7f8653091b65cadb02dba45ac6806d9adbe3a138
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-29 10:14:20 +0530

    :lipstick: Update code snippet border

--- components/common/CodeSnippet.tsx
+++ components/common/CodeSnippet.tsx
@@ -8,12 +8,12 @@ function CodeSnippetComponent({ snippet: { code, language } }: { snippet: CodeSn
     <Highlight {...defaultProps} code={code} language={language} theme={nightOwl}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre
-          className={`${className} text-left my-4 p-2 overflow-x-auto rounded border border-solid border-gray-400`}
+          className={`${className} text-left my-4 p-4 overflow-x-auto rounded-lg border-solid border-8 border-gray-600`}
           style={style}
         >
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })} className="table-row" key={i}>
-              <span className="table-cell text-right pr-4 select-none opacity-50">{i + 1}</span>
+              {/* <span className="table-cell text-right pr-4 select-none opacity-50">{i + 1}</span> */}
               <span className="table-cell">
                 {line.map((token, key) => (
                   <span {...getTokenProps({ token, key })} key={key} />
