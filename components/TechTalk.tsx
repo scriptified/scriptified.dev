208330411451c4fc4e798eec7c9c46d42a73b217
Prateek Surana <prateeksurana3255@gmail.com>
2020-07-29 09:53:48 +0530

    :sparkles: Added tags to talks

--- components/TechTalk.tsx
+++ components/TechTalk.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 import Talk from '../interfaces/talk';
 import Text from './common/Text';
+import Tags from './common/Tags';
 
 // TODO: Create a loading indicator for this
 const ReactPlayer = dynamic(() => import('react-player/lazy'), { loading: (): JSX.Element => <p>Loading...</p> });
@@ -13,6 +14,7 @@ const TechTalk = ({ techTalk }: { techTalk: Talk }): JSX.Element => {
       </Text>
       <ReactPlayer url={techTalk.talkURL} controls={true} pip={true} />
       <Text additionalStyles="mt-4">{techTalk.desc}</Text>
+      <Tags tags={techTalk.tags} />
     </div>
   );
 };
