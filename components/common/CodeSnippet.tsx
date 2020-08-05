37b069b972bf03042967fcf179e17fe9ee6f5615
gupta-ji6 <ayushgupta197@gmail.com>
2020-08-05 11:54:57 +0530

    Removed #15 code

--- components/common/CodeSnippet.tsx
+++ components/common/CodeSnippet.tsx
@@ -1,19 +1,19 @@
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import nightOwl from 'prism-react-renderer/themes/nightOwl';
 
 import CodeSnippet from '../../interfaces/codeSnippet';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
 
 function CodeSnippetComponent({ snippet: { code, language } }: { snippet: CodeSnippet }): JSX.Element {
   return (
     <Highlight {...defaultProps} code={code} language={language} theme={nightOwl}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre
-          className={`${className} text-left my-4 p-4 overflow-x-auto rounded-lg border-solid border-8 border-gray-600`}
+          className={`${className} text-left my-4 p-2 overflow-x-auto rounded border border-solid border-gray-400`}
           style={style}
         >
           {tokens.map((line, i) => (
             <div {...getLineProps({ line, key: i })} className="table-row" key={i}>
-              {/* <span className="table-cell text-right pr-4 select-none opacity-50">{i + 1}</span> */}
+              <span className="table-cell text-right pr-4 select-none opacity-50">{i + 1}</span>
               <span className="table-cell">
                 {line.map((token, key) => (
                   <span {...getTokenProps({ token, key })} key={key} />
