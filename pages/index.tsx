b7e62a69aaad57cd3a608bfbdac1620240e7cd59
gupta-ji6 <ayushgupta197@gmail.com>
2020-08-23 18:11:33 +0530

    :sparkles: Updated landing page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
-import Head from 'next/head';
-import Tilt from 'react-parallax-tilt';
-import { GetStaticProps } from 'next';
-import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
+
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import SubscribeCard from '../components/common/SubscribeCard';
+import Text from '../components/common/Text';
+import Tilt from 'react-parallax-tilt';
 import { getAllIssuesMeta } from '../lib/issues';
+
 // import utilStyles from "../styles/utils.module.css";
 
 export default function Home({
@@ -14,23 +18,38 @@ export default function Home({
     title: string;
     id: string;
   }[];
-}) {
+}): JSX.Element {
+  const reversedIssuesData = allIssuesData.slice(0, 3).reverse();
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className="text-lg leading-normal">
-        <Tilt>
-          <div className="flex justify-center w-full">
-            <img src="/images/scriptified-logo.gif" />
-          </div>
+      <section className="text-lg flex flex-col items-center leading-normal bg-green-500 h-screen">
+        <Tilt
+          tiltReverse
+          transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+          className="flex flex-col items-center justify-center mt-6"
+        >
+          {/* <div className="w-1/3"> */}
+          <img src="/images/scriptified-logo-green.gif" className="w-1/4" />
+          {/* </div> */}
+          <Text type="h1" color="gray-0" additionalStyles="text-6xl">
+            Scriptified
+          </Text>
+          <Text type="h1" color="gray-1" additionalStyles="mb-20 lg:mb-24 text-center">
+            Your Goto JavaScript Newsletter
+          </Text>
         </Tilt>
+        <div className="sm:mx-8 lg:w-2/4">
+          <SubscribeCard homePage />
+        </div>
       </section>
-      <section className="text-lg leading-normal pt-px mt-64">
-        <h2 className="text-2xl leading-snug my-4 mx-0">Issues</h2>
+      <section className="sm:px-16 md:px-64 lg:px-64 text-lg leading-normal mt-16">
+        <h2 className="text-2xl leading-snug my-4 mx-0">Latest Issues</h2>
         <ul className="m-0 p-0 list-none">
-          {allIssuesData.map(({ id, desc, title }) => (
+          {reversedIssuesData.map(({ id, desc, title }) => (
             <li className="mt-0 mx-0 mb-5" key={id}>
               <Link href="/issues/[id]" as={`/issues/${id}`}>
                 <a className="text-green-600 font-sans font-semibold text-2xl no-underline hover:underline">{title}</a>
