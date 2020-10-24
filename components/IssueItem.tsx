1717def72e13a8497a852f644d4364d2a0cbf888
gupta-ji6 <ayushgupta197@gmail.com>
2020-10-25 00:59:53 +0530

    :lipstick: Add icon with title

--- components/IssueItem.tsx
+++ components/IssueItem.tsx
@@ -14,17 +14,21 @@ const IssueItem = ({
   const theme = useThemeState();
   return (
     <div className="my-20 relative z-10">
-      <Text type="h1" color="text-black" additionalStyles="relative z-10">
+      <Text type="h1" color="text-black" additionalStyles="relative z-10 flex flex-row">
+        {React.cloneElement(icon, {
+          color: `text-${theme}-300`,
+          additionalStyles: 'h-12 w-12 -ml-2 mr-4',
+        })}
         {title}
       </Text>
       <div className="mt-1">
         {children}
-        <div className="absolute h-24 w-24 issue-item-icon hidden xl:block">
+        {/* <div className="absolute h-24 w-24 issue-item-icon hidden xl:block">
           {React.cloneElement(icon, {
             color: `text-${theme}-300`,
             additionalStyles: 'h-24 w-24',
           })}
-        </div>
+        </div> */}
       </div>
     </div>
   );
