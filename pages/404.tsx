103084c5be05d38d1ffb958079df5f1dd09a9a67
gupta-ji6 <ayushgupta197@gmail.com>
2020-11-20 20:14:57 +0530

    :goal_net: Update custom 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,19 @@
-export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+import Head from 'next/head';
+import Text from '../components/common/Text';
+import Layout, { siteTitle } from '../components/Layout';
+import { useThemeState } from '../theme/ThemeContext';
+
+export default function Custom404(): JSX.Element {
+  const theme = useThemeState();
+  return (
+    <Layout>
+      <Head>
+        <title>{`${siteTitle} - Page Not Found`}</title>
+      </Head>
+      <section className={`min-h-screen flex flex-col bg-gradient-to-br from-${theme}-600 to-${theme}-900`}>
+        <Text type="h1">404</Text>
+        <Text type="h3">Page Not Found</Text>
+      </section>
+    </Layout>
+  );
 }
