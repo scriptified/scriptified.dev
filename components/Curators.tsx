69837c5fe830effe9550a84645d00a1f543d78c5
gupta-ji6 <ayushgupta197@gmail.com>
2020-11-20 22:43:37 +0530

    :sparkles: Update curators desc & design

--- components/Curators.tsx
+++ components/Curators.tsx
@@ -6,7 +6,7 @@ import Text from './common/Text';
 const CURATORS = [
   {
     name: 'Ayush Gupta',
-    desc: 'description',
+    desc: 'React Native Developer at FirstCry. Mobile and Web App Developer, Amateur Photographer and Blogger.',
     imgURL: 'https://github.com/gupta-ji6.png',
     links: {
       website: 'https://ayushgupta.tech',
@@ -17,7 +17,7 @@ const CURATORS = [
   },
   {
     name: 'Prateek Surana',
-    desc: 'description',
+    desc: `Building Devfolioco && Fold Bank | Frontend engineer | Technical Writer | Loves JS`,
     imgURL: 'https://github.com/prateek3255.png',
     links: {
       website: 'https://prateeksurana.me/',
@@ -27,6 +27,15 @@ const CURATORS = [
     },
   },
 ];
+
+// reference - https://javascript.info/task/shuffle
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+// shuffle(CURATORS);
 
 const Curators = (): JSX.Element => {
   const theme = useThemeState();
@@ -41,43 +50,53 @@ const Curators = (): JSX.Element => {
           Curators
         </Text>
         <Text additionalStyles="mt-4 max-w-2xl text-xl leading-7 lg:mx-auto" color={`text-${theme}-600`} type="base">
-          A look at what&apos;s inside our newsletter
+          The two nerdy devs who curate this newsletter for you
         </Text>
       </div>
-      <div className="flex justify-evenly sm:flex-col md:flex-row flex-wrap space-y-8 sm:space-y-8 sm:space-x-8 md:space-y-0">
-        {CURATORS.map((curator, index) => {
-          return (
-            <div className="flex sm:flex-col md:flex-row flex-wrap" key={index}>
-              <div
-                className={`relative mr-4 mb-4 p-1 bg-gradient-to-br from-${theme}-300 to-${theme}-700 rounded transition duration-300`}
-              >
-                <div className={`bg-${theme}-100 p-1 rounded`}>
-                  <img
-                    src={curator.imgURL}
-                    loading="lazy"
-                    className={`w-48 h-48 bg-gradient-to-br from-${theme}-300 to-${theme}-500 rounded shadow-2xl transform transition duration-700 hover:scale-105 hover:shadow-lg`}
-                    alt={curator.name}
-                  />
+      <div className="flex justify-evenly items-center flex-col flex-wrap">
+        <div className="grid px-0 lg:px-12">
+          {CURATORS.map((curator, index) => {
+            return (
+              <div className="flex sm:flex-col md:flex-row flex-wrap mb-8" key={index}>
+                <div
+                  className={`flex-shrink-1 mr-4 mb-4 p-1 bg-gradient-to-br from-${theme}-300 to-${theme}-700 rounded transition duration-300`}
+                >
+                  <div className={`bg-${theme}-100 p-1 rounded`}>
+                    <a href={curator.links.website} target="_blank" rel="noreferrer">
+                      <img
+                        src={curator.imgURL}
+                        loading="lazy"
+                        className={`w-48 h-48 bg-gradient-to-br from-${theme}-300 to-${theme}-500 rounded shadow-2xl transform transition duration-700 hover:scale-105 hover:shadow-lg`}
+                        alt={curator.name}
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <a
+                    href={curator.links.website}
+                    target="_blank"
+                    rel="noreferrer"
+                    className={`text-${theme}-800 hover:underline`}
+                  >
+                    <Text
+                      type="h1"
+                      color="text-transparent"
+                      additionalStyles={`bg-gradient-to-b from-${theme}-600 to-${theme}-800 bg-clip-text`}
+                      inline
+                    >
+                      {curator.name}
+                    </Text>
+                  </a>
+                  <Text type="base" color={`text-${theme}-700`} inline additionalStyles="py-4">
+                    {curator.desc}
+                  </Text>
+                  <SocialLinks links={curator.links} additionalStyles="space-x-3" />
                 </div>
               </div>
-              <div className="flex flex-col">
-                <a href={curator.links.website} target="_blank" rel="noreferrer">
-                  <Text
-                    type="h1"
-                    color="text-transparent"
-                    additionalStyles={`bg-gradient-to-b from-${theme}-600 to-${theme}-800 bg-clip-text hover:underline`}
-                  >
-                    {curator.name}
-                  </Text>
-                </a>
-                <Text color={`text-${theme}-700`} additionalStyles="py-4">
-                  {curator.desc}
-                </Text>
-                <SocialLinks links={curator.links} additionalStyles="space-x-3" />
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
