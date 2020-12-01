679516efa700720700b2cb0d05392a0849e163cb
gupta-ji6 <ayushgupta197@gmail.com>
2020-12-01 13:15:43 +0530

    :wrench: Update next config to use withPWA HOF

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true, // let this plugin register service worker for you
+    sw: 'service-worker.js', // service worker script file name
+    publicExcludes: ['images'], // an array of glob pattern strings to exclude files in the public folder from being precached
+    // disable: process.env.NODE_ENV === 'development', // disable pwa feature in development
+    // scope: '/', // url scope for pwa
+    // runtimeCaching, // caching strategies
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -10,4 +21,4 @@ module.exports = {
 
     return config;
   },
-};
+});
