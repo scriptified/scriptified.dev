7459dd75509692e218f1ca5ed853a513ff55d593
gupta-ji6 <ayushgupta197@gmail.com>
2020-12-05 18:32:19 +0530

    :art: Create seperate componentr for Hero section

---
+++ components/HeroSection.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { Fragment } from 'react';
+import Tilt from 'react-parallax-tilt';
+import { useThemeState } from '../theme/ThemeContext';
+import SubscribeCard from './common/SubscribeCard';
+import Text from './common/Text';
+
+const HeroSection = (): JSX.Element => {
+  const theme = useThemeState();
+  return (
+    <Fragment>
+      <Tilt
+        tiltReverse
+        transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+        className="flex flex-col items-center justify-center mt-6 relative z-10"
+      >
+        <div className="w-1/3">
+          <Image
+            src="/images/scriptified-logo.png"
+            width={150}
+            height={150}
+            className="w-1/4"
+            alt="logo"
+            layout="responsive"
+          />
+        </div>
+        <Text type="h1" color={`text-${theme}-100`} additionalStyles="text-6xl">
+          Scriptified
+        </Text>
+        <Text type="h1" color={`text-${theme}-200`} additionalStyles="mb-16 sm:mb-12 lg:mb-16 text-center">
+          Your Goto JavaScript Newsletter
+        </Text>
+      </Tilt>
+      <div className="w-5/6 sm:mx-8 lg:w-2/4 mb-2 relative z-10">
+        <SubscribeCard homePage />
+      </div>
+    </Fragment>
+  );
+};
+
+export default HeroSection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,17 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useEffect } from 'react';
-import Tilt from 'react-parallax-tilt';
 import Trianglify from 'trianglify';
 import { colors } from 'tailwindcss/defaultTheme';
 
-import SubscribeCard from '../components/common/SubscribeCard';
 import Layout, { siteTitle } from '../components/Layout';
-import Text from '../components/common/Text';
 import { getAllIssuesMeta } from '../lib/issues';
 import { useThemeState } from '../theme/ThemeContext';
 import FeatureSection from '../components/FeatureSection';
 import Meta from '../interfaces/meta';
 import LatestIssues from '../components/LatestIssues';
 import Curators from '../components/Curators';
-
-// ============= Component ================
+import HeroSection from '../components/HeroSection';
 
 export default function Home({ allIssuesData }: { allIssuesData: Meta[] }): JSX.Element {
   const theme = useThemeState();
@@ -59,31 +54,7 @@ export default function Home({ allIssuesData }: { allIssuesData: Meta[] }): JSX.
         id="section"
         className={`text-lg flex flex-col items-center leading-normal bg-${theme}-500 pb-4 px-4 relative`}
       >
-        <Tilt
-          tiltReverse
-          transitionEasing="cubic-bezier(.03,.98,.52,.99)"
-          className="flex flex-col items-center justify-center mt-6 relative z-10"
-        >
-          <div className="w-1/3">
-            <Image
-              src="/images/scriptified-logo.png"
-              width={150}
-              height={150}
-              className="w-1/4"
-              alt="logo"
-              layout="responsive"
-            />
-          </div>
-          <Text type="h1" color="text-gray-100" additionalStyles="text-6xl">
-            Scriptified
-          </Text>
-          <Text type="h1" color="text-gray-200" additionalStyles="mb-16 sm:mb-12 lg:mb-16 text-center">
-            Your Goto JavaScript Newsletter
-          </Text>
-        </Tilt>
-        <div className="w-5/6 sm:mx-8 lg:w-2/4 mb-2 relative z-10">
-          <SubscribeCard homePage />
-        </div>
+        <HeroSection />
       </section>
       <section className={`mx-auto px-8 sm:px-16 md:px-40 lg:px-64 text-lg leading-normal py-16 bg-${theme}-100`}>
         <LatestIssues allIssuesData={allIssuesData} />
