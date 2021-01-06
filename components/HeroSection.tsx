007a26db304c50f8a3646e91f73cecf75dc7ec18
gupta-ji6 <ayushgupta197@gmail.com>
2021-01-07 02:02:16 +0530

    :lipstick: Make logo responsive in hero

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -14,16 +14,16 @@ const HeroSection = (): JSX.Element => {
         transitionEasing="cubic-bezier(.03,.98,.52,.99)"
         className="flex flex-col items-center justify-center mt-6 relative z-10"
       >
-        <div className="flex justify-center">
-          <ScriptifiedLogo color={`text-${theme}-100`} additionalStyles="w-1/3 h-1/3" />
+        <div className="flex justify-center h-24 w-24 md:h-32 md:w-32 mt-4 sm:mt-6 lg:mt-12">
+          <ScriptifiedLogo color={`text-${theme}-100`} additionalStyles="h-24 w-24 md:h-32 md:w-32" />
         </div>
         <Text type="h1" color={`text-${theme}-100`} additionalStyles="text-6xl">
           Scriptified
         </Text>
-        <Text type="h1" color={`text-${theme}-200`} additionalStyles="mb-10 sm:mb-12 lg:mb-16 text-center">
-          Your Goto JavaScript Newsletter
-        </Text>
       </Tilt>
+      <Text type="h1" color={`text-${theme}-200`} additionalStyles="mb-10 sm:mb-12 lg:mb-16 text-center">
+        Your Goto JavaScript Newsletter
+      </Text>
       <div className="w-5/6 sm:mx-8 lg:w-2/4 mb-2 relative z-10">
         <SubscribeCard homePage />
       </div>
