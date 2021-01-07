9fade89d26ed3bcfd336e9781e1845279140eeec
Prateek Surana <prateeksurana3255@gmail.com>
2021-01-07 21:14:25 +0530

    :lipstick: Hide android icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,7 +37,7 @@ class MyDocument extends Document {
           <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
           <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
           <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png"></link>
-          <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+          {/* <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" /> */}
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
