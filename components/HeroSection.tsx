c466f2fbd608a9e073667dd052ee1b12518670f8
gupta-ji6 <ayushgupta197@gmail.com>
2021-02-13 16:51:02 +0530

    :lipstick: Avoid awkward line-break in description

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,6 +5,15 @@ import Text from './common/Text';
 import { ScriptifiedLogo } from './icons/icons';
 import { siteConfig } from './Layout';
 
+/*
+ ref => https://twitter.com/wesbos/status/1306668630709600262?s=20
+ to avoid awkward line-break b/w "React and JavaScript"
+*/
+const numberOfImpWords = 3;
+const desciptionArray = siteConfig.description.split(' ');
+const descPart1 = desciptionArray.slice(0, desciptionArray.length - numberOfImpWords).join(' ');
+const descPart2 = desciptionArray.slice(-1 * numberOfImpWords).join(' ');
+
 const HeroSection = (): JSX.Element => {
   const theme = useThemeState();
   return (
@@ -20,9 +29,11 @@ const HeroSection = (): JSX.Element => {
       <Text
         // type="h1"
         color={`text-${theme}-200`}
-        additionalStyles="text-3xl mb-10 sm:mb-12 lg:mb-16 max-w-3xl text-center"
+        additionalStyles="text-3xl mb-10 sm:mb-12 lg:mb-16 max-w-3xl text-center px-4"
       >
-        <h2>{siteConfig.description}</h2>
+        <h2>
+          {`${descPart1} `} <span className="whitespace-no-wrap">{descPart2}</span>
+        </h2>
       </Text>
       <div className="w-5/6 sm:mx-8 lg:w-2/4 mb-2 relative z-10">
         <SubscribeCard homePage />
