d049c297cc183218355757c840966f28d75b39fa
Prateek Surana <prateeksurana3255@gmail.com>
2021-02-15 17:33:41 +0530

    :hammer: Load google analytics only on development environment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,10 +36,12 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
 
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -48,8 +50,10 @@ class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
-            }}
-          />
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
