6f8a021a6225fb6fecbe9697a8e816b832ac33ac
gupta-ji6 <ayushgupta197@gmail.com>
2021-03-06 12:36:04 +0530

    :adhesive_bandage: :iphone: Fix button loader on phone

--- components/common/SubscribeCard.tsx
+++ components/common/SubscribeCard.tsx
@@ -130,7 +130,7 @@ const SubscribeCard = ({ homePage = false }: { homePage?: boolean }): JSX.Elemen
           <Button
             size="md"
             type={homePage ? 'primary' : 'basic'}
-            additionalStyles={`rounded shadow w-3/6 self-center mt-4`}
+            additionalStyles={`rounded shadow min-w-1/2 w-1/2 self-center mt-4`}
             buttonAttributes={{ type: 'submit' }}
             loading={loading}
           >
