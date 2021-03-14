8b0e4bb1333820bf3ad738b1e64d91709209d65f
gupta-ji6 <ayushgupta197@gmail.com>
2021-03-14 17:25:47 +0530

    :lipstick: Decrease issue # size in issue list item

--- components/common/IssueListItem.tsx
+++ components/common/IssueListItem.tsx
@@ -16,16 +16,16 @@ const IssueListItem = ({ issueData }: { issueData: Meta }): JSX.Element => {
   return (
     <div className={`flex py-8 pr-2 max-w-3xl md:border-0 border-b border-${theme}-500`} key={number}>
       <div className="h-full flex md:flex-row flex-col items-start">
-        <div className="w-16 flex-shrink-0 flex md:flex-col text-center leading-none mb-2">
-          <span className={`text-${theme}-700 -mb-1 text-lg md:mr-0 mr-2 font-bold md:order-2 uppercase`}>Issue</span>
+        <div className="w-16 flex-grow flex md:flex-col text-center leading-none mb-2">
+          <span className={`text-${theme}-700 text-lg md:mr-0 mr-2 font-bold md:order-2 uppercase`}>Issue</span>
           <span
-            className={`text-transparent bg-gradient-to-b from-${theme}-400 to-${theme}-600 bg-clip-text font-bold text-lg md:text-6xl leading-none`}
+            className={`flex-1 text-transparent bg-gradient-to-b from-${theme}-400 to-${theme}-600 bg-clip-text font-bold text-lg md:text-5xl leading-none`}
           >
-            #{number}
+            {`#${number}`}
           </span>
         </div>
 
-        <div className={`flex-1 md:pl-3 md:ml-6 md:border-l md:border-${theme}-400`}>
+        <div className={`md:pl-3 md:ml-6 md:border-l md:border-${theme}-400`}>
           <Link href="/issues/[number]" as={`/issues/${number}`}>
             <a className={`text-${theme}-600 font-sans font-semibold text-2xl no-underline hover:underline`}>
               <Text
