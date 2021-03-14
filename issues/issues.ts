b8d0fa7063b58ff0801951f994a2ef306a5a32f3
Prateek Surana <prateeksurana3255@gmail.com>
2021-03-14 17:37:15 +0530

    🎉  Added issue #2 (#43)
    * :tada: Added issue #2
    
    * :bento: Add issue 2 OG
    
    * :bento: :adhesive_bandage: Update date in issue-2 OG
    
    * 🔨 Create a script for generate email templates
    
    * :lipstick: Update ToolItem img shadow & design
    
    * :lipstick: Decrease issue # size in issue list item
    
    Co-authored-by: gupta-ji6 <ayushgupta197@gmail.com>

--- issues/issues.ts
+++ issues/issues.ts
@@ -135,7 +135,153 @@ console.log(numbers[3])`,
       title: 'Toasts, Styled Components and Writing Legible Code Comments',
       desc:
         'Learn the mental model you need for styled-components, how JavaScript sets can help you, display beautiful toasts, improve code comments, and do you really understand the sort method.',
-      imgURL: 'https://scriptified.dev/images/issue-page-og.jpg',
+      imgURL: 'https://scriptified.dev/images/issue-1/issue-page-og.jpg',
+    },
+  },
+  {
+    tipOfTheWeek: {
+      snippet: {
+        code: `<head>
+  <base href="https://scriptified.dev" target="_blank">
+</head>
+...
+<!-- This link will automatically be opened in a new tab -->
+<a href="issue/2">Scriptified Issue #2</a>`,
+        language: 'html',
+      },
+      desc:
+        'You can open all links in a document in a new tab without adding `target=_blank` to every link with the [`<base>`](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/base) element. You can also specify a base path using the `href` atrribute which will be used for all the relative URLs.',
+      sourceName: 'Google',
+      sourceURL: 'https://google.com',
+      tags: ['HTML'],
+    },
+
+    articles: [
+      {
+        title: 'Before you memo()',
+        url: 'https://overreacted.io/before-you-memo/',
+        desc:
+          'You must have used `React.memo` or the memoization hooks like `useCallback` and friends for improving the performance of your components, but do you know there are much simpler ways by which you can optimize for performance without using any memoization.',
+        author: 'Dan Abramov',
+        tags: ['React', 'Optimization'],
+      },
+      {
+        title: 'Using fetch with TypeScript',
+        url: 'https://kentcdodds.com/blog/using-fetch-with-type-script',
+        desc:
+          'Migrating existing JavaScript files to TypeScript can sometimes be a pain in the ass, especially when they include network requests. In this article, Kent converts a JavaScript file with network requests using fetch to TypeScript, step by step discussing some practices you can follow while migrating.',
+        author: 'Kent C. Dodds',
+        tags: ['React', 'TypeScript'],
+      },
+    ],
+
+    tools: [
+      {
+        title: 'Meta Tags',
+        url: 'https://metatags.io/',
+        logo: '/images/issue-2/meta-tags.svg',
+        desc:
+          'A one-click meta-tags generator with previews for Google, Facebook, Twitter, LinkedIn, Pinterest and Slack.',
+        author: 'Meta tags',
+        tags: ['SEO'],
+      },
+      {
+        title: 'Remotion',
+        url: 'https://remotion.dev',
+        logo: '/images/issue-2/remotion.png',
+        desc:
+          'Use the magic of CSS, SVGs and WebGL to create professionally designed videos with React 17 and TypeScript. It also allows you to preview your the video in the browser with fast refresh and Timelines for frames.',
+        author: 'Jonny Burger',
+        tags: ['React'],
+      },
+    ],
+
+    quiz: {
+      question: 'What is the output of the below snippet?',
+      snippet: {
+        code: `const w = 12;
+const x = "undefined";
+const y = null;
+const z = { name: "scriptified" };
+
+const answer = w && x && y && z;
+
+console.log(answer);`,
+        language: 'js',
+      },
+      options: [
+        {
+          id: 1,
+          text: '`false`',
+          description:
+            "Although the answer is a [falsy](https://developer.mozilla.org/en-US/docs/Glossary/Falsy) value but it is not false, hope that's a good enough hint for you ;)",
+        },
+        {
+          id: 2,
+          text: '`{ name: "scriptified" }`',
+          description:
+            'We think you should read [falsy values in JavaScript](https://developer.mozilla.org/en-US/docs/Glossary/Falsy) and then try again!',
+        },
+        {
+          id: 3,
+          text: '12',
+          description:
+            'Have a look at [short circuit evaluation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND#short-circuit_evaluation) in JavaScript and then try again!',
+        },
+        {
+          id: 4,
+          text: '`null`',
+          description:
+            'Voila! This is an example of [short circuit evaluation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND#short-circuit_evaluation) with [logical AND operator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_AND). It evaluated from left to right and if the first expression is [truthy](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) then it moves to the next one, and it keeps doing this until it either finishes in which it returns the last value or reaches a falsy value and returns that instead (null is that falsy value in our case)',
+        },
+      ],
+      answerId: 4,
+    },
+
+    // devTip by devOfTheWeek
+    // you can extract any github user's profile image by this link - https://github.com/user-name.png
+
+    devOfTheWeek: {
+      name: 'Ameer Jhan',
+      profileImg: 'https://github.com/ameerthehacker.png',
+      profileLink: {
+        website: 'https://ameerthehacker.me/',
+        github: 'https://github.com/ameerthehacker',
+        twitter: 'https://twitter.com/ameerthehacker',
+      },
+      bio:
+        'Ameer is a speaker, writer, AWS Solution Architect who has authored & contributed in several popular open source projects like styled-wind & react-lazyload. He recently launched [Blazepack](https://github.com/ameerthehacker/blazepack) -  a super fast dev server powered by CodeSandbox sandpack bundler, which is catching a lot of eyes.',
+    },
+
+    talks: [
+      {
+        talkURL: 'https://www.youtube.com/watch?v=u2WtILkz0fI',
+        title: 'Navigating the Hype Driven Frontend Development World',
+        desc:
+          "The obsession to always move on to the latest and greatest, and the fear of missing out while working with something that's older than a few months is currently dominating the dev world. Checkout how Kitze demonstrates everyone's struggles with the hype driven frontend development.",
+        tags: ['Frontend'],
+      },
+    ],
+
+    website: {
+      name: 'Github',
+      URL: 'https://github.com',
+      desc: 'Home for Devlopers',
+    },
+
+    gif: {
+      gifURL: '/images/issue-2/this-week.gif',
+      caption:
+        'When you try to debug your code in production! (via [r/ProgrammerHumor](https://www.reddit.com/r/ProgrammerHumor/comments/m4299h/when_you_debug_in_production/))',
+    },
+
+    meta: {
+      number: 2,
+      dateOfPublishing: '2021-03-14',
+      title: 'Creating videos with React, Fetching with TypeScript and the Hype Driven Frontend Development',
+      desc:
+        'Explore how to create professional videos with React, optimize your React code without using memo, handle data fetching with TypeScript and open all links in new tabs with bare minimum code.',
+      imgURL: 'https://scriptified.dev/images/issue-2/og.png',
     },
   },
 ];
