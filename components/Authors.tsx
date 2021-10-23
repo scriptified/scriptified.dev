286372f934c5dcdb458570f46ee52c06295e9b30
Ayush Gupta <ayushgupta197@gmail.com>
2021-10-23 14:01:00 +0530

    :recycle: refactor/author: add separate Author component

---
+++ components/Authors.tsx
@@ -0,0 +1,24 @@
+import Author from '../interfaces/author';
+import Text from './common/Text';
+
+function Authors({ authors }: { authors: Author[] }): JSX.Element {
+  return (
+    <div className="pb-2">
+      <Text inline type="small" color="text-gray-600" additionalStyles="py-2 pr-1">
+        by
+      </Text>
+      {authors?.map((author, index) => {
+        const isLastElement = index === authors.length - 1;
+        return (
+          <a href={author.website} target="_blank" rel="noreferrer" key={author.id}>
+            <Text inline additionalStyles="uppercase hover:underline" type="small" color="text-gray-600">
+              {isLastElement ? author.name : `${author.name}, `}
+            </Text>
+          </a>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Authors;
