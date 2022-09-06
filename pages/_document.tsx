2aa007ab4a096f8436f75e139b1d3e8ff0577868
hussamkhatib <hussamkhatib@gmail.com>
2022-09-06 14:44:26 +0530

    fix: remove redundant <meta charSet .../> tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="utf-8" />
           <meta name="application-name" content={siteConfig.name} />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
