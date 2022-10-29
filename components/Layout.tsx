cc12c1fe98094457facd1d15cca9b14d848d9530
mohammed hussam <52914487+hussamkhatib@users.noreply.github.com>
2022-10-29 15:40:04 +0530

    fix: remove redundant <meta charSet .../> tag (#101)
    * fix: remove redundant <meta charSet .../> tag
    
    * fix(Layout): remove extra cahrset meta tag
    
    * empty commit to re-run checks
    
    Co-authored-by: hussamkhatib <hussamkhatib@gmail.com>
    Co-authored-by: Ayush Gupta <ayushgupta197@gmail.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -51,7 +51,6 @@ const Head = ({
     <meta name="title" content={title} />
 
     {/* Essentials */}
-    <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     {/* General */}
